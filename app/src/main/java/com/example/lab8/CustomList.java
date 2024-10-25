1e2d4240ddc367fad4734f16f35951ac75643aeb
Reuben John <rjjohn1@ualberta.ca>
2024-10-25 10:27:36 -0600

    updated deleteCity

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -65,6 +65,7 @@ public class CustomList extends ArrayAdapter<City> {
         return cities.contains(city);
     }
     public void deleteCity(City city) {
+        cities.remove(city);
     }
 
 }
